5eccc6987a36a195aad620b877fcfdce94e3e8f0
DoctorAcc22 <105140626+DoctorAcc22@users.noreply.github.com>
2023-10-27 16:29:10 +0700

    654565

--- api/app/Http/Controllers/AmenityController.php
+++ api/app/Http/Controllers/AmenityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Amenity;
 use Illuminate\Support\Facades\Validator;
+use App\Actions\Randomizer\RandomizerActions;
 
 class AmenityController extends Controller
 {
@@ -34,8 +35,61 @@ class AmenityController extends Controller
         ]);
     }
 
-    public function update()
+    public function update(Amenity $amenity)
     {
+        $validator = Validator::make(request()->all(), [
+            'code' => 'required',
+            'name' => 'required',
+            'status' => 'required',
+        ]);
 
+        $code = request()->code;
+        if ($code == 'AUTO') {
+            do {
+                $code = $this->generateUniqueCode();
+            } while (! $this->isUniqueCode($code,  $amenity->id));
+        } else {
+            if (! $this->isUniqueCode($code, $amenity->id)) {
+                return response()->error([
+                    'code' => 'Code already used.',
+                ], 422);
+            }
+        }
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->all()[0],
+            ]);
+        }
+
+        $amenity->code = request()->code;
+        $amenity->name = request()->name;
+        $amenity->status = request()->status;
+        $amenity->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Amenity has been updated.',
+        ]);
+    }
+
+    public function generateUniqueCode(): string
+    {
+        $rand = app(RandomizerActions::class);
+        $code = $rand->generateAlpha().$rand->generateNumeric();
+
+        return $code;
+    }
+
+    public function isUniqueCode(string $code, ?int $exceptId = null): bool
+    {
+        $result = Amenity::where('code', '=', $code);
+
+        if ($exceptId) {
+            $result = $result->get()->where('id', '<>', $exceptId);
+        }
+
+        return $result->count() == 0 ? true : false;
     }
 }
--- api/tests/Feature/AmenityAPITest.php
+++ api/tests/Feature/AmenityAPITest.php
@@ -24,15 +24,48 @@ class AmenityAPITest extends TestCase
         $api = $this->json('POST', 'api/login', array_merge($user->toArray(), ['password' => $password]));
 
         $api->assertStatus(200);
-
         
         $amenityArr = [
-            'code' => '001',
+            'code' => strtoupper(fake()->lexify()).fake()->numerify(),
             'name' => 'School',
             'status' => 1,
         ];
 
         $api = $this->json('POST', 'api/amenity/save', $amenityArr);
+
+        $api->assertSuccessful();
+        $this->assertDatabaseHas('amenities', [
+            'code' => $amenityArr['code'],
+            'name' => $amenityArr['name'],
+            'status' => $amenityArr['status'],
+        ]);
+    }
+
+    public function test_amenity_api_call_update_expect_successfull()
+    {
+        $amenity = new Amenity();
+        $amenity->code = strtoupper(fake()->lexify()).fake()->numerify();
+        $amenity->name = 'Hospital';
+        $amenity->status = 1;
+        $amenity->save();
+
+        $password = '1234567890';
+        $user = User::factory()->create(['password' => $password]);
+
+        $this->actingAs($user);
+        $user['password'] = $password;
+
+        $api = $this->json('POST', 'api/login', array_merge($user->toArray(), ['password' => $password]));
+
+        $api->assertStatus(200);
+
+        $amenityArr = [
+            'code' => strtoupper(fake()->lexify()).fake()->numerify(),
+            'name' => 'School X',
+            'status' => 1,
+        ];
+        
+        $api = $this->json('POST', 'api/amenity/edit/' . $amenity->id, $amenityArr);
 
         $api->assertSuccessful();
         $this->assertDatabaseHas('amenities', [
