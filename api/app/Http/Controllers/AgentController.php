6341287d2fe55c5d6e1dc99b89d41f94d2f9367f
HartantoSoraya <80240259+HartantoSoraya@users.noreply.github.com>
2023-10-27 13:37:13 +0700

    asd

--- api/app/Http/Controllers/AgentController.php
+++ api/app/Http/Controllers/AgentController.php
@@ -60,8 +60,67 @@ class AgentController extends Controller
         ]);
     }
 
-    public function update()
+    public function update($id)
     {
+        // Validasi input
+        $validator = Validator::make(request()->all(), [
+            'code' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+            'phone_number' => 'required',
+            'date_of_birth' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip' => 'required',
+            'hire_date' => 'required',
+            'employment_status' => 'required',
+            'commision_rate' => 'required',
+            'specialization' => 'required',
+            'years_of_experience' => 'required',
+            'properties_sold' => 'required',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->all()[0],
+            ]);
+        }
+
+        $agent = Agent::find($id);
+
+        if (!$agent) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Agent not found.',
+            ]);
+        }
+
+        $agent->code = request()->code;
+        $agent->first_name = request()->first_name;
+        $agent->last_name = request()->last_name;
+        $agent->email = request()->email;
+        $agent->phone_number = request()->phone_number;
+        $agent->date_of_birth = request()->date_of_birth;
+        $agent->address = request()->address;
+        $agent->city = request()->city;
+        $agent->state = request()->state;
+        $agent->zip = request()->zip;
+        $agent->hire_date = request()->hire_date;
+        $agent->employment_status = request()->employment_status;
+        $agent->commision_rate = request()->commision_rate;
+        $agent->specialization = request()->specialization;
+        $agent->years_of_experience = request()->years_of_experience;
+        $agent->properties_sold = request()->properties_sold;
+        
+        $agent->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Agent has been updated.',
+        ]);
     }
+
 }
